ee5c9d96e33ac22b58b320d1b51ca9b8b7c61bcd
klee3721 <94339608+klee3721@users.noreply.github.com>
2023-08-18 17:35:32 +0700

    Update web.php

--- Faketube/routes/web.php
+++ Faketube/routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/channels', [ChannelController::class, 'index'])->name('channels.index');
+Route::get('/channels/create', [ChannelController::class, 'create'])->name('channels.create');
+Route::post('/channels', [ChannelController::class, 'store'])->name('channels.store');
+Route::get('/channels/{channel}', [ChannelController::class, 'show'])->name('channels.show');
+Route::get('/channels/{channel}/edit', [ChannelController::class, 'edit'])->name('channels.edit');
+Route::put('/channels/{channel}', [ChannelController::class, 'update'])->name('channels.update');
+Route::delete('/channels/{channel}', [ChannelController::class, 'destroy'])->name('channels.destroy');
